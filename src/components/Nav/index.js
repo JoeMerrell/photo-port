c5b5acc6fa3d4e958469f81b9bb559721f12fd5a
Joseph <joe@uhhuhohyeah.com>
2021-08-25 17:47:47 -0700

    still need to render images - can't find bug

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const {
@@ -7,38 +7,42 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
+
   return (
     <header className="flex-row px-1">
-      
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
-        
         <ul className="flex-row">
           <li className="mx-2">
             <a data-testid="about" href="#about">
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                currentCategory.name === category.name
                 }`}
               key={category.name}
             >
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
@@ -52,6 +56,3 @@ function Nav(props) {
 }
 
 export default Nav;
-
-
-// Whenever we map over anything in JSX, the outermost element must have a key attribute that's set to be something unique. This helps React keep track of items in the virtual DOM.
