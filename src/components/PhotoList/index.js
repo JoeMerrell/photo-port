2533bb7b94dabfdd4493fd6f9997de9debf92b8b
Joseph <joe@uhhuhohyeah.com>
2021-08-30 09:12:13 -0700

    fixed image display issue in PhotoList - index.js

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import image1 from "../../assets/small/commercial/1.jpg";
 const PhotoList = ({ category }) => {
 
   const [photos] = useState([
@@ -107,7 +107,8 @@ const PhotoList = ({ category }) => {
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             key={image.name}
